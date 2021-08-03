2e67f26defb7dc2cf908100398f594e390293566
almada gaspar <almada3d@gmail.com>
2021-08-03 18:21:09 -0300

    Realizando Proyecto Individual

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -5,41 +5,76 @@ const axios = require('axios');
 require('dotenv').config();    // Importo la configuración de 'dotenv' para poder acceder a las constantes del archivo .env
 const { API_KEY } = process.env;   // Almaceno en API_KEY la key para poder acceder a la API al usar axios.
 
-const { Recipe } = require('../db.js');     //// Importo el modelo que necesito haciendo un destructuring.
+const { Recipe, Diet } = require('../db.js');     // Importo los modelos que necesito mediante destructuring.
 
 
 // Ej: http://localhost:3001/recipe/3245
 
 router.get('/:id', async (req, res) => {
+    if (req.params.id.includes('-')) {   // Si el id de la receta que debo buscar contiene un guión, debo buscarlo en mi base de datos.
 
-    // ¡¡¡ FALTARIA CONSIDERAR LAS RECETAS DE MI BDD !!!!
+        try {
+            let dbRecipe = await Recipe.findByPk(req.params.id,  // Busco en mi base de datos una receta con el id recibido.
+                {
+                    // Incluir a la receta encontrada, el nombre de las dietas a las que está relacionada, en una propieda 'diets'. 
+                    include: {
+                        model: Diet,
+                        attributes: ['name'],
+                        through: { attributes: [] } // Esto evita que se incluyan los IDs de la tabla intermedia.
+                    }
+                })
+            
+            //  Si encontré una receta con el id recibido, cambio la estructura de la propiedad incluida 'diets' para que sea un array de strings en lugar de un array de objetos.
+            //  "diets": [ { "name": "Ketogenic" }, { "name": "Pescetarian" } ]       =====>     "diets": ["Ketogenic", "Pescetarian" ]
+            if (dbRecipe) {      
+                    const recipeToSend = {
+                        id: dbRecipe.id,
+                        name: dbRecipe.name,
+                        summary: dbRecipe.summary,
+                        score: dbRecipe.score,
+                        healthScore: dbRecipe.healthScore,
+                        instructions: dbRecipe.instructions,
+                        createdInDb: dbRecipe.createdInDb,
+                        diets: dbRecipe.diets && dbRecipe.diets.map(d => d.name)
+                    }
+                return res.json(recipeToSend) // Envio al Front la receta encontrada.
+            }
+            // Si NO encontre una receta con ese id, envío el mensaje correspondiente.
+            res.status(404).json({ error: 'No se encontró una receta con ese id en la base de datos.' })
 
-    try {   // Uso try-catch por si desde el Front se solicita detalles de una receta con un id que no existe en la API externa.
-        const recipeAPI = await axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=${API_KEY}`)
-        const recipe = recipeAPI.data;
-
-        // Las dietas a las que pertenece cada receta estan en dos lugares distintos, por lo que tengo que unirlas pero evitando que se repitan.
-        let initialDiets = [];
-        if (recipe.vegetarian && !recipe.diets.includes('vegetarian')) initialDiets.push('vegetarian');
-        if (recipe.vegan && !recipe.diets.includes('vegan')) initialDiets.push('vegan');
-        if (recipe.glutenFree && !recipe.diets.includes('gluten free')) initialDiets.push('gluten free');
-
-        // Con la información recibida de la API externa, creo la receta a enviar usando solo la informacion requerida.
-        const recipeToSend = {
-            image: recipe.image,
-            name: recipe.title,
-            score: recipe.spoonacularScore,
-            healthScore: recipe.healthScore,
-            dishTypes: recipe.dishTypes,
-            summary: recipe.summary,
-            instructions: recipe.instructions,
-            diets: initialDiets.concat(recipe.diets),  // Junto todas las dietas de esta receta.
-            //  ES CORRECTO EL MODO EN EL QUE INCLUI LOS TIPO DE DIETA ASOCIADOS???
+        } catch (error) {
+            // Si el id recibido no tiene el formato de UUID a pesar de tener un '-', envío el mensaje correspondiente.
+            res.status(404).json({ error: 'El id recibido no es valido.' })
         }
-        res.json(recipeToSend);
 
-    } catch (error) {   // Si NO se encontró una receta con el id enviado del Front. 
-        return res.status(404).json({ error: 'No se encontró una receta con ese id' })
+    } else {    // Si el id recivido NO contiene un guión, debo buscarlo en la API externa.
+
+        try {   // Uso try-catch por si desde el Front se solicita detalles de una receta con un id que no existe en la API externa.
+            const recipeAPI = await axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=${API_KEY}`)
+            const recipe = recipeAPI.data;
+
+            // Las dietas a las que pertenece cada receta estan en dos lugares distintos, por lo que tengo que unirlas pero evitando que se repitan.
+            let initialDiets = [];
+            if (recipe.vegetarian && !recipe.diets.includes('vegetarian')) initialDiets.push('vegetarian');
+            if (recipe.vegan && !recipe.diets.includes('vegan')) initialDiets.push('vegan');
+            if (recipe.glutenFree && !recipe.diets.includes('gluten free')) initialDiets.push('gluten free');
+
+            // Con la información recibida de la API externa, creo la receta a enviar usando solo la informacion requerida.
+            const recipeToSend = {
+                image: recipe.image,
+                name: recipe.title,
+                score: recipe.spoonacularScore,
+                healthScore: recipe.healthScore,
+                dishTypes: recipe.dishTypes,
+                summary: recipe.summary,
+                instructions: recipe.instructions,
+                diets: initialDiets.concat(recipe.diets),  // Junto todas las dietas de esta receta.
+            }
+            res.json(recipeToSend);
+
+        } catch (error) {   // Si NO se encontró una receta con el id recibido del Front, envío el mensaje correspondiente.
+            return res.status(404).json({ error: 'No se encontró una receta con ese id en la API externa.' })
+        }
     }
 })
 
@@ -47,10 +82,11 @@ router.get('/:id', async (req, res) => {
 
 
 router.post('/', async (req, res) => {
+    // Creo una receta en mi base de datos, con los datos que llegan desde el Front, enviados por body.
+    const { name, summary, score, healthScore, instructions, diets } = req.body;    // Tomo del body la información que necesito para crear la nueva receta.
 
-    const { name, summary, score, healthScore, instructions, diets } = req.body;
-
-    // En este caso NO es necesario enviar un valor para el campo 'createdInDb' ya que se autocompleta automaticamente en 'true'.
+    // Agrego a la tabla 'recipes' una nueva receta. En este caso NO es necesario
+    // enviar un valor para el campo 'createdInDb' ya que se autocompleta automaticamente en 'true'.
     const newRecipe = await Recipe.create({
         name: name,
         summary: summary,
@@ -58,15 +94,10 @@ router.post('/', async (req, res) => {
         healthScore: healthScore,
         instructions: instructions,
     })
-    await newRecipe.addDiets(diets);
-    
+    await newRecipe.addDiets(diets);     // Agrego en la tabla intermedia 'recipe_diet' un registro (recipeId y dietID) por cada genero al que pertenece esta nueva receta.
+
     res.json(newRecipe)
-
 })
-
-
-
-
 
 
 
--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -5,15 +5,37 @@ const axios = require('axios');
 require('dotenv').config();    // Importo la configuración de 'dotenv' para poder acceder a las constantes del archivo .env
 const { API_KEY } = process.env;   // Almaceno en API_KEY la key para poder acceder a la API al usar axios.
 
+const { Recipe, Diet } = require('../db.js');     // Importo los modelos que necesito mediante destructuring.
 
-// Ej:  http://localhost:3001/recipes?query=apple
+
+// Ej:  http://localhost:3001/recipes?name=apple
 // Ej:  http://localhost:3001/recipes
 
 router.get('/', async (req, res) => {
     // Si se envió del Front un nombre de receta para la busqueda, retorno las 100 recetas cuyo nombre incluya la palabra recibida.
     if (req.query.name) {
         //// ¡¡¡ FALTARIA CONSIDERAR E INCLUIR LAS RECETAS DE MI BDD !!!!
-        recipesAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100&query=${req.query.name}`)
+        const dbRecipe = await Recipe.findAll({
+            // where: {
+            //     name: req.query.name
+            // },
+            attributes: ['id','name', 'createdInDb']
+
+        });
+
+        console.log('RECETAS DE LA BASE DE DATOS',dbRecipe)
+
+        let recipeByName = await dbRecipe.filter(recipe => recipe.name.toLowerCase().includes(req.query.name.toLocaleLowerCase()))
+
+        recipeByName = recipeByName.recipe.dataValues
+        console.log('RECETAS DE LA BASE DE DATOS FILTRADAS POR NOMBRE',recipeByName)
+
+
+
+
+
+
+        const recipesAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100&query=${req.query.name}`)
 
         const recipesToSend = recipesAPI.data.results && recipesAPI.data.results.map(recipe => {
             // Las dietas a las que pertenece cada receta estan en dos lugares distintos, por lo que tengo que unirlas pero evitando que se repitan.
@@ -30,15 +52,39 @@ router.get('/', async (req, res) => {
             }
         })
 
-    
-        if (recipesToSend.length) return res.json(recipesToSend)
+
+        if (recipesToSend.length) return res.json(recipeByName.concat(recipesToSend))
         res.status(404).send({ error: 'No se encontraron recetas con ese nombre' })
 
     } else {   // Si NO se envió del Front un nombre de receta para la busqueda, retorno las 100 primeras recetas que me envie la API externa.
-       
-        // Para que la cantidad de recetas devueltas por la API sea 100 en lugar de solo 10, agrego el flag: &number=100
+
+        const dbRecipes = await Recipe.findAll({      // Busco todas las dietas de mi base de datos.
+            attributes: ['id', 'name', 'createdInDb'],    // Solo quiero estos campos.
+            include: {                                       // Incluto las dietas relacionadas a esta receta.
+                model: Diet,
+                attributes: ['name'],
+                through: { attributes: [] } // Esto evita que se incluyan los IDs de la tabla intermedia.
+            }
+        })
+
+
+        //  Si encontré recetas en mi base de datos, cambio la estructura de la propiedad incluida 'diets' para que sea un array de strings en lugar de un array de objetos. 
+        //  "diets": [ { "name": "Ketogenic" }, { "name": "Pescetarian" } ]       =====>     "diets": ["Ketogenic", "Pescetarian" ]
+        let dbRecipesFormated = [];
+        // if (dbRecipes.length) {   // BORRAR ESTE FOR SI NO HAY ERRORES CON MI BASE DE DATOS VACIA.
+        for (let i = 0; i < dbRecipes.length; i++) {
+            let recipeFormated = {         // Esta variable representa cada elemento del array de recetas encontradas en la base de datos
+                id: dbRecipes[i].id,
+                name: dbRecipes[i].name,
+                createdInDb: dbRecipes[i].createdInDb,
+                diets: dbRecipes[i].diets && dbRecipes[i].diets.map(d => d.name)
+            }
+            dbRecipesFormated.push(recipeFormated)
+        }
+        // }
+
         // Para obtener mayor información sobre las recetas, como por ejemplo el tipo de dieta, agrego el flag: &addRecipeInformation=true
-        recipesAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
+        const recipesAPI = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
 
         const recipesToSend = recipesAPI.data.results.map(recipe => {
             // Las dietas a las que pertenece cada receta estan en dos lugares distintos, por lo que tengo que unirlas pero evitando que se repitan.
@@ -54,7 +100,9 @@ router.get('/', async (req, res) => {
                 diets: extraDiets.concat(recipe.diets)  // Junto todas las dietas de esta receta.
             }
         })
-        return res.json(recipesToSend);    //// Respondo con un array con 100 juegos (imágen, nombre y sus géneros)
+
+        // Respondo con las recetas encontradas en mi base de datos concatenadas con las encontradas en la API.
+        return res.json(dbRecipesFormated.concat(recipesToSend));
     }
 })
 
